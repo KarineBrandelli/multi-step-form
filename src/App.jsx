77dd07dc061401ebab9d6c6c9cae3fc8d39bde22
Karine Brandelli <karinebrandelli@live.com>
2023-03-05 18:06:34 -0300

    build: import Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './index.css'
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import "./index.css";
 
 export default function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
-};
+    <div className="flex border-2 rounded-xl p-2 w-3/4 h-3/4 bg-white">
+      <Sidebar />
+    </div>
+  );
+}
