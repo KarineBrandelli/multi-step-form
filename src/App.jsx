2dc11342e0e6efb617bd0b338bde4d54d0a8beda
Karine Brandelli <karinebrandelli@live.com>
2023-03-06 16:23:14 -0300

    build: set container to Step1 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Step1 } from "./components/Steps/Step1";
 import "./index.css";
 
 export default function App() {
   return (
     <div className="flex border-2 rounded-xl p-2 w-3/4 h-3/4 bg-white">
       <Sidebar />
+      <div className="border-2 px-4 mx-auto my-auto h-[38rem] w-[40rem] flex flex-col justify-between">
+        <Step1 />
+      </div>
     </div>
   );
 }
