47c922e85bc515b33cab83f52ebc0195bba3d31a
Karine Brandelli <karinebrandelli@live.com>
2023-03-07 19:32:37 -0300

    build: create helper to Step 2 plans

---
+++ src/components/Steps/helpers/plansStep2.js
@@ -0,0 +1,24 @@
+import arcadeIcon from "../../../assets/icon-arcade.svg";
+import advancedIcon from "../../../assets/icon-advanced.svg";
+import proIcon from "../../../assets/icon-pro.svg";
+
+export const plansStep2 = [
+  {
+    name: "Arcade",
+    icon: arcadeIcon,
+    monthlyPrice: 9,
+    yearlyPrice: 90,
+  },
+  {
+    name: "Advanced",
+    icon: advancedIcon,
+    monthlyPrice: 12,
+    yearlyPrice: 120,
+  },
+  {
+    name: "Pro",
+    icon: proIcon,
+    monthlyPrice: 15,
+    yearlyPrice: 150,
+  },
+];
