a781a96db33526d529165c42bfe9247c5c9c57d7
Karine Brandelli <karinebrandelli@live.com>
2023-03-09 13:05:38 -0300

    build: create step 5 component and add responsivity

---
+++ src/components/Steps/Step5.jsx
@@ -0,0 +1,15 @@
+export const Step5 = () => {
+  return (
+    <>
+      <div className="my-auto text-center grid place-items-center">
+        <img src="https://user-images.githubusercontent.com/108953489/223235284-17759e76-1bab-400e-b107-7f2c77b40a4b.svg"></img>
+        <h4 className="font-bold text-2xl lg:text-4xl text-blue-marine mt-8 mb-4">Thank you!</h4>
+        <p className="text-gray-cool lg:text-lg">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@karine.com.
+        </p>
+      </div>
+    </>
+  );
+};
