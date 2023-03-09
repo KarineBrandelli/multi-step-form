a6d7dd9834ebb37d99edec81a0e203afff5eecf9
Karine Brandelli <karinebrandelli@live.com>
2023-03-09 13:12:41 -0300

    build: import Steps and Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { Header } from "./components/Header/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Step1 } from "./components/Steps/Step1";
+import { Step2 } from "./components/Steps/Step2";
+import { Step3 } from "./components/Steps/Step3";
+import { Step4 } from "./components/Steps/Step4";
+import { Step5 } from "./components/Steps/Step5";
 import "./index.css";
 
 export default function App() {
   return (
-    <div className="flex border-2 rounded-xl p-2 w-3/4 h-3/4 bg-white">
-      <Sidebar />
-      <div className="border-2 px-4 mx-auto my-auto h-[38rem] w-[40rem] flex flex-col justify-between">
-        <Step1 />
+    <>
+      <Header />
+      <div className="flex rounded-xl -mt-24 px-3 py-6 lg:p-2 w-[93%] h-[550px] lg:w-3/4 lg:h-3/4 lg:mt-0 bg-white">
+        <Sidebar />
+        <div className="mx-auto my-auto w-[98%] h-full lg:px-4 lg:h-[38rem] lg:w-[65%] flex flex-col justify-between">
+          <Step1 />
+          {/* <Step2 /> */}
+          {/* <Step3 /> */}
+          {/* <Step4 /> */}
+          {/* <Step5 /> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
