a277b2662d0c4a7917cbe09658ba69e5070d6f0f
Karine Brandelli <karinebrandelli@live.com>
2023-03-12 10:12:39 -0300

    build: add Routes to form steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import { Header } from "./components/Header/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Step1 } from "./components/Steps/Step1";
@@ -5,22 +7,26 @@ import { Step2 } from "./components/Steps/Step2";
 import { Step3 } from "./components/Steps/Step3";
 import { Step4 } from "./components/Steps/Step4";
 import { Step5 } from "./components/Steps/Step5";
+
 import "./index.css";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <div className="flex rounded-xl -mt-24 px-3 py-6 lg:p-2 w-[93%] h-[550px] lg:w-3/4 lg:h-3/4 lg:mt-0 bg-white">
-        <Sidebar />
-        <div className="mx-auto my-auto w-[98%] h-full lg:px-4 lg:h-[38rem] lg:w-[65%] flex flex-col justify-between">
-          <Step1 />
-          {/* <Step2 /> */}
-          {/* <Step3 /> */}
-          {/* <Step4 /> */}
-          {/* <Step5 /> */}
+      <BrowserRouter>
+        <Header />
+        <div className="flex rounded-xl -mt-24 px-3 py-6 lg:p-2 w-[93%] h-[550px] lg:w-3/4 lg:h-3/4 lg:mt-0 bg-white">
+          <Sidebar />
+          <div className="mx-auto my-auto w-[98%] h-full lg:px-4 lg:h-[38rem] lg:w-[65%] flex flex-col justify-between">
+            <Routes>
+              <Route path="/" element={<Step1 />} />
+              <Route path="/Step2" element={<Step2 />} />
+              <Route path="/Step3" element={<Step3 />} />
+              <Route path="/Step4" element={<Step4 />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     </>
   );
 }
