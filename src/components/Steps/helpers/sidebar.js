03520ff1969059d4952719db22addfbd291ec8d8
Karine Brandelli <karinebrandelli@live.com>
2023-03-12 10:14:24 -0300

    build: add route to sidebar object

--- src/components/Steps/helpers/sidebar.js
+++ src/components/Steps/helpers/sidebar.js
@@ -2,17 +2,21 @@ export const sidebar = [
   {
     stepTitle: "YOUR INFO",
     stepCounter: 1,
+    route: "/"
   },
   {
     stepTitle: "SELECT PLAN",
     stepCounter: 2,
+    route: "/Step2"
   },
   {
     stepTitle: "ADD-ONS",
     stepCounter: 3,
+    route: "/Step3"
   },
   {
     stepTitle: "SUMMARY",
     stepCounter: 4,
+    route: "/Step4"
   },
 ];
