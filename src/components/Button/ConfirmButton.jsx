e0c8d411de63c872efcc9e6575a2e77ba5c481af
Karine Brandelli <karinebrandelli@live.com>
2023-03-13 14:09:47 -0300

    build: create components for the buttons

---
+++ src/components/Button/ConfirmButton.jsx
@@ -0,0 +1,9 @@
+export const ConfirmButton = () => {
+  return (
+    <button
+      type="submit"
+      className="bg-blue-purplish text-gray-light border-2 border-blue-purplish rounded-lg py-2 lg:py-4 w-24 lg:w-40 text-sm lg:text-base font-bold hover:opacity-80 transition" >
+      Confirm
+    </button>
+  );
+};
---
+++ src/components/Button/GoBackButton.jsx
@@ -0,0 +1,9 @@
+export const GoBackButton = () => {
+  return (
+    <div className="flex justify-between">
+      <button className="text-gray-cool border-2 rounded-lg py-2 lg:py-4 w-24 lg:w-40 text-sm lg:text-base font-bold  hover:text-blue-marine transition">
+        Go Back
+      </button>
+    </div>
+  );
+};
