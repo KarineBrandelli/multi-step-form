6bea2b0fe80ae2ad2388c40fed659ef9428c7159
Karine Brandelli <karinebrandelli@live.com>
2023-03-14 13:28:59 -0300

    build: adjust function parameter

--- src/components/Button/NextStepButton.jsx
+++ src/components/Button/NextStepButton.jsx
@@ -1,7 +1,8 @@
-export const NextStepButton = () => {
+export const NextStepButton = ({ handleClick }) => {
   return (
     <button
       type="submit"
+      onClick={handleClick}
       className="bg-blue-purplish text-gray-light border-2 border-blue-purplish rounded-lg py-2 lg:py-4 w-24 lg:w-40 text-sm lg:text-base font-bold hover:opacity-80 transition" >
       Next Step
     </button>
