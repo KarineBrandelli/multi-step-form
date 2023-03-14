ed98cc4832f797b0e493a39162d773ec2fa2e713
Karine Brandelli <karinebrandelli@live.com>
2023-03-14 13:28:13 -0300

    build: adjust button route

--- src/components/Steps/Step1.jsx
+++ src/components/Steps/Step1.jsx
@@ -130,12 +130,7 @@ export const Step1 = () => {
       </form>
 
       <div className="flex justify-end">
-        <button
-          type="submit"
-          onClick={handleSubmit(handleRegistration, handleError)}
-          className="bg-blue-purplish text-gray-light border-2 border-blue-purplish rounded-lg py-2 lg:py-4 w-24 lg:w-40 text-sm lg:text-base font-bold hover:opacity-80 transition">
-          Next Step
-        </button>
+        <NextStepButton handleClick={handleSubmit(handleRegistration)} />
       </div>
     </>
   );
