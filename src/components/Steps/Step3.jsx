664127ca4c6bb18330e6b74e97a034d509bf68fe
Karine Brandelli <karinebrandelli@live.com>
2023-03-14 13:32:47 -0300

    build: update import file name 'plansData'

--- src/components/Steps/Step3.jsx
+++ src/components/Steps/Step3.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { GoBackButton } from "../Button/GoBackButton";
 import { NextStepButton } from "../Button/NextStepButton";
 
-import { plansStep3 } from "./helpers/plansStep3";
+import { plansData } from "./helpers/plansData";
 
 export const Step3 = () => {
   return (
@@ -15,27 +15,27 @@ export const Step3 = () => {
           Add-ons help enhance your gaming experience.
         </p>
 
-        {plansStep3.map((ao, index) => (
+        {plansData.map((plan, index) => (
           <div
             key={index}
             className="px-1 py-2 lg:px-8 lg:py-6 rounded-lg flex items-center justify-between mb-3 cursor-pointer border-[1px] border-gray-cool hover:ring-[1px] hover:ring-blue-purplish hover:border-[1px] hover:border-blue-purplish transition duration-300" >
             <div className="flex justify-between items-center gap-3 lg:gap-8">
               <input
-                value={ao}
+                value={plan}
                 type="checkbox"
                 className="lg:h-6 lg:w-6 accent-blue-purplish" />
 
               <div>
                 <h3 className="text-blue-marine font-medium text-sm lg:text-lg">
-                  {ao.title}
+                  {plan.title}
                 </h3>
                 <span className="text-gray-cool text-xs lg:text-base">
-                  {ao.desc}
+                  {plan.description}
                 </span>
               </div>
             </div>
             <span className="text-blue-purplish text-xs lg:text-base font-medium">
-              +${ao.monthlyPrice}/mon
+              +${plan.monthlyPrice}/mon
             </span>
           </div>
         ))}
