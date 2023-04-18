06f2a96c752d1c932e246790d51b0f53912451d0
Karine Brandelli <karinebrandelli@live.com>
2023-04-18 08:59:18 -0300

    build: change helper file name

---
+++ src/components/Steps/helpers/plansStep3.js
@@ -0,0 +1,20 @@
+export const plansStep3 = [
+  {
+    title: "Online service",
+    description: "Access to multiplayer games",
+    monthlyPrice: 1,
+    yearlyPrice: 10,
+  },
+  {
+    title: "Larger storage",
+    description: "Extra 1TB of cloud save",
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+  {
+    title: "Customizable Profile",
+    description: "Custom theme on your profile",
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+];
