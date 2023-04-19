5fedd5281460b77e0bde4d522d6364c271a76d43
Karine Brandelli <karinebrandelli@live.com>
2023-04-19 13:22:24 -0300

    build: remove import for Link, update function arguments and remove onClick

--- src/components/Sidebar/Navigation.jsx
+++ src/components/Sidebar/Navigation.jsx
@@ -1,15 +1,12 @@
-import { Link } from "react-router-dom";
-
-export const Navigation = ({ route, isActive, stepCounter, stepTitle, onClick }) => {
+export const Navigation = ({ isActive, stepCounter, stepTitle }) => {
   return (
     <>
       <li className="mb-8">
         <div className="flex items-center">
-            <div
-              onClick={onClick}
-              className={`${
-                isActive
-                  ? " text-blue-marine bg-gradient-to-r from-blue-light to-blue-pastel border-none"
+          <div
+            className={`${
+              isActive
+                ? " text-blue-marine bg-gradient-to-r from-blue-light to-blue-pastel border-none"
                 : "bg-transparent border-white"
             }
                   w-10 h-10 rounded-[50%] lg:mr-4 font-bold flex items-center justify-center border-2 border-white`} >
