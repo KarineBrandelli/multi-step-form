e1c3f245cacc4403f58c97ac7578342bdfa372b6
Karine Brandelli <karinebrandelli@live.com>
2023-04-19 13:19:38 -0300

    style: remove cursor-pointer from Navigation

--- src/components/Sidebar/Navigation.jsx
+++ src/components/Sidebar/Navigation.jsx
@@ -11,10 +11,11 @@ export const Navigation = ({ route, isActive, stepCounter, stepTitle, onClick })
               className={`${
                 isActive
                   ? " text-blue-marine bg-gradient-to-r from-blue-light to-blue-pastel border-none"
-                  : "bg-transparent border-white" }
-                  w-10 h-10 rounded-[50%] lg:mr-4 font-bold flex items-center justify-center cursor-pointer border-2 border-white`} >
-              {stepCounter}
-            </div>
+                : "bg-transparent border-white"
+            }
+                  w-10 h-10 rounded-[50%] lg:mr-4 font-bold flex items-center justify-center border-2 border-white`} >
+            {stepCounter}
+          </div>
           </Link>
           <div className="hidden lg:block">
             <span className="block text-sm text-gray-light">
