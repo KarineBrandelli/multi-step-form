40f932ce30f8ebad4084e76bf966a090f7c7a022
Karine Brandelli <karinebrandelli@live.com>
2023-04-19 13:15:14 -0300

    build: remove handleClick function from Navigation

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -14,10 +14,6 @@ export const Sidebar = () => {
     setActive(active.map((_, i) => i === currentRoute))
   }, [])
 
-  const handleClick = (index) => {
-    setActive(active.map((_, i) => i === index))
-  }
-
   return (
     <>
       <div className="w-full flex justify-center -mt-24 mb-7 lg:mt-0 lg:h-full lg:w-72 lg:mr-2 lg:block lg:p-0 lg:rounded-xl lg:bg-[url('../../src/assets/bg-sidebar-desktop.svg')] bg-center bg-no-repeat bg-cover">
@@ -28,7 +24,6 @@ export const Sidebar = () => {
                 key={`route-${index + 1}`}
                 route={item.route}
                 isActive={active[index]}
-                onClick={() => handleClick(index)}
                 stepCounter={item.stepCounter}
                 stepTitle={item.stepTitle}
               />
