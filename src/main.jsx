df9918beed760b53f54603d80a0b2c187b4f5d96
Karine Brandelli <karinebrandelli@live.com>
2023-04-21 19:35:20 -0300

    build: import AppContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AppContextProvider } from "./context/AppContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </React.StrictMode>
+);
