fe9ac03a773886d63fbbc11eefa443e6bd069da5
Karine Brandelli <karinebrandelli@live.com>
2023-05-09 09:44:45 -0300

    build: replace img path for url

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 export const Header = () => {
   return (
-    <div className="lg:hidden bg-[url('../../src/assets/bg-sidebar-mobile.svg')] py-20 w-full bg-center bg-no-repeat bg-cover flex justify-center"></div>
+    <div className="lg:hidden bg-[url('https://user-images.githubusercontent.com/108953489/236943566-5f1cf9d2-93e5-4b08-9bb1-5a13133632c2.svg')] py-20 w-full bg-center bg-no-repeat bg-cover flex justify-center"></div>
   );
 };
